fcc0b055438d860db11b5c561cb93dfcc36efc3e
manasirugle <manasirugle0211@gmail.com>
2025-09-14 16:12:42 +0530

    Initial commit: Added backend Spring Boot project

---
+++ backend/src/test/java/com/herway/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herway.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
